490c13cfb7acaca2e6ae1e8122a1bec107474a37
Sasho Hribernik <sasho.hribernik@gmail.com>
2020-11-27 13:08:32 +0000

    MERN stack setup for deployment

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require("./db/mongoose");
 const userRouter = require("./routers/user");
 const diaryRouter = require("./routers/diary");
 const cors = require("cors");
+const path = require("path");
 
 const port = process.env.PORT;
 const app = express();
@@ -14,9 +15,20 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
 app.use(userRouter);
 app.use(diaryRouter);
 
+// Serve React app
+app.get("/", async (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+app.use(express.static("client/build"));
+
 app.listen(port, () => {
-  console.log(`App listening at port: ${port}`);
+  console.log(`Server listening at port: ${port}`);
 });
